08f83451aca953112e26b41f2870a5780f1ae7a0
salamandery <sp.ront@hotmal.com>
2022-10-03 22:37:09 -0300

    UnitMovement, Transform sync

--- Assets/Assets/Scripts/Networking/NetworkManagerBehaviour.cs
+++ Assets/Assets/Scripts/Networking/NetworkManagerBehaviour.cs
@@ -4,20 +4,16 @@ using UnityEngine;
 public class NetworkManagerBehaviour : NetworkManager
 {
     #region Server
-    [Server]
     public override void OnClientConnect()
     {
-        base.OnClientConnect();
-
         Debug.Log($"A Player was connected");
+        base.OnClientConnect();
     }
 
-    [Server]
     public override void OnServerAddPlayer(NetworkConnectionToClient conn)
     {
-        base.OnServerAddPlayer(conn);
-
         Debug.Log($"{numPlayers} Players in the server!");
+        base.OnServerAddPlayer(conn);
     }
     #endregion
 }
---
+++ Assets/Assets/Scripts/Units/UnitsMovements.cs
@@ -0,0 +1,49 @@
+using Mirror;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class UnitsMovements : NetworkBehaviour
+{
+    [SerializeField]
+    private NavMeshAgent agent;
+    private Camera mainCamera;
+
+    #region Server
+    [Command]
+    private void CmdMove(Vector3 position)
+    {
+        bool NavHitCondition = NavMesh.SamplePosition(
+            position,
+            out NavMeshHit hit,
+            1f,
+            NavMesh.AllAreas
+        );
+        if (!NavHitCondition) { return; }
+
+        agent.SetDestination(hit.position);
+    }
+    #endregion
+    #region Client
+    public override void OnStartAuthority()
+    {
+        mainCamera = Camera.main;
+    }
+
+    private void OnMoveAction()
+    {
+        if (!hasAuthority) { return; }
+        if (!Input.GetMouseButtonDown(1)) { return; }
+
+        Ray ray = mainCamera.ScreenPointToRay(Input.mousePosition);
+
+        if (!Physics.Raycast(ray, out RaycastHit hit, Mathf.Infinity)) { return; }
+
+        CmdMove(hit.point);
+    }
+
+    void Update()
+    {
+        OnMoveAction();
+    }
+    #endregion
+}
